03c9de90a434cf4b70410612859fc9b4da86d989
kinakomoch7 <book-insect14@outlook.jp>
2023-08-07 23:42:07 +0900

    貸し出し履歴を参照するシステム作成

--- Library.java
+++ Library.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Library {
     List<Book> shelf = new ArrayList<Book>();
     List<User> manager = new ArrayList<User>();
+    Map<Book, List<History>> historyMap = new HashMap<Book, List<History>>();
 
     void run() {
         this.printWelcomeMessage();
 
-        this.addBooks(shelf);
+        this.addBooks();
         this.registerUsers();
 
         Iterator<User> iterator = manager.iterator();
@@ -19,15 +23,90 @@ public class Library {
             System.out.printf("%s %s, %d%n", aUser.name, aUser.gender, aUser.age);
         }
 
-        List<Book> findedBook = this.findAnd("羅生門", "芥川龍之介", "青空文庫", 1997);
-
-        for (Book book : findedBook) {
-            this.remove(book);
-        }
         for (Book book : shelf) {
             printBook(book);
         }
 
+        runLend();
+
+        runFindHistory(findHistory(null, null));
+    }
+
+    void runFindHistory(List<History> histories) {
+        for (History hist : histories) {
+            System.out.println(hist);
+        }
+    }
+
+    List<History> findHistory(Book book, User user) {
+        if (book != null) {
+            for (Map.Entry<Book, List<History>> entry : historyMap.entrySet()) {
+                if (Objects.equals(book, entry.getKey())) {
+                    return entry.getValue();
+                }
+            }
+        } else {
+            for (List<History> list : historyMap.values()) {
+                System.out.print(list);
+            }
+        }
+        return null;
+    }
+
+    void runLend() {
+        // User user1 = this.createUser("小出朋希", "男", 21);
+        // User user2 = this.createUser("小出朋希", "男", 21);
+        // Book book1 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
+        // Book book2 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
+        // Book book3 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
+        // Book book4 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
+
+        History history1 = this.createHistory(manager.get(1), shelf.get(1));
+        History history2 = this.createHistory(manager.get(0), shelf.get(2));
+        History history3 = this.createHistory(manager.get(1), shelf.get(3));
+    }
+
+    History lend(User user, Book book) {
+        History createdHistory = createHistory(user, book);
+        if (registerHistory(createdHistory)) {
+            return createdHistory;
+        } else {
+            return null;
+        }
+    }
+
+    boolean registerHistory(History history) {
+        List<History> histories = historyMap.get(history.book);
+        if (histories == null) {
+            histories = new ArrayList<History>();
+            historyMap.put(history.book, histories);
+        }
+        if (this.canLend(history, histories)) {
+            histories.add(history);
+            return true;
+        }
+        return false;
+    }
+
+    Boolean canLend(History history,
+            List<History> histories) {
+        if (!shelf.contains(history.book)) {
+            return false;
+        }
+        if (histories.size() > 0) {
+            History lastHistory = histories.get(histories.size() - 1);
+            if (lastHistory.isLent()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    History createHistory(User user, Book book) {
+        HistoryCreator createHistory = new HistoryCreator();
+        History history = createHistory.createHistory(book, user);
+        history.lendDate = new Date();
+        return history;
     }
 
     void registerUsers() {
@@ -115,7 +194,7 @@ public class Library {
         System.out.printf("%s (%s) %s, %d%n", book.title, book.authors, book.publisher, book.publishYear);
     }
 
-    void addBooks(List<Book> shelf) {
+    void addBooks() {
         Book book1 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
         shelf.add(book1);
         Book book2 = this.createBook("羅生門", "芥川龍之介", "青空文庫", 1997);
