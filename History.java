5bb446f1c129a0125a6a2237255454b149172843
kinakomoch7 <book-insect14@outlook.jp>
2023-08-07 23:44:43 +0900

    同じく

--- History.java
+++ History.java
@@ -9,8 +9,23 @@ public class History {
     void print() {
         System.out.printf("%s, %s, %s, %d ", book.title, book.authors, book.publisher, book.publishYear);
 
+        String str;
+        if (!isLent()) {
+            str = "(貸し出し中)";
+        } else {
+            str = "(配架中)";
+        }
+        System.out.print(str);
+
         System.out.printf("%s(%s, %d) ", user.name, user.gender, user.age);
 
         System.out.printf("%s ~ %s\n", String.valueOf(lendDate), String.valueOf(returnDate));
+    }
+
+    boolean isLent() {
+        if (returnDate == null) {
+            return false;
+        }
+        return true;
     }
 }
